a00a6d68b95b48cc14b3b0a837fbfda29912d4fa
XTrandi <XTrandi@users.noreply.github.com>
2016-12-05 12:46:48 +0100

    Adds Javadoc comments

--- Robot/src/parkingRobot/hsamr10/Navigation/StraightLine.java
+++ Robot/src/parkingRobot/hsamr10/Navigation/StraightLine.java
@@ -14,14 +14,20 @@ import java.util.LinkedList;
 import java.util.List;
 //import java.util.Collection;
 
+/**
+ * class to handle line specific calculations for each part of the map
+ * 
+ * @author Tung Le
+ *
+ */
 public class StraightLine {
 	private Direction direction;
 	private StraightLine followingLine;
 	private double startCoordinate;
 	private double endCoordinate;
 	private double constCoordinate;
-	public boolean conditionAngle = false;
-	public boolean conditionDistance = false;
+	private boolean conditionAngle = false;
+	private boolean conditionDistance = false;
 	public int id;
 	//private LinkedList<ParkingSlot> parkingSlots;
 	private List<Double> parkingFrontPoints;
@@ -29,16 +35,27 @@ public class StraightLine {
 	private List<Integer> parkingID;
 	static private final double OFFSET_PARKSLOT_BACKSIDE  = 0.100;
 	static private final double OFFSET_PARKSLOT_FRONTSIDE = 0.075;
-	
+	/**
+	 * used for monitoring purposes
+	 */
 	public IMonitor monitor = null;
-	
+	/**
+	 * defines the direction a robot can follow a line
+	 * 
+	 * @author Tung Le
+	 *
+	 */
 	public enum Direction {
 		NORTH,
 		EAST,
 		SOUTH,
 		WEST
 	}
-	
+	/**
+	 * Class constructor. Defines the lines direction according to start and target points
+	 * 
+	 * @param line  line given as an four-dimensional array with starting coordinates x1, y1 and target coordinates x2, y2
+	 */
 	public StraightLine(Line line) {		
 		if ( line.getX1() == line.getX2() ) {
 			this.startCoordinate = line.getY1()/100;
@@ -59,7 +76,10 @@ public class StraightLine {
 		this.parkingID		 	= new LinkedList<Integer>();
 		//this.parkingSlots = new LinkedList<ParkingSlot>();
 	}
-	
+	/**
+	 * sets the following line. Method to be called once.
+	 * @param line  line given as an four-dimensional array with starting coordinates x1, y1 and target coordinates x2, y2
+	 */
 	public void setFollowingLine(StraightLine line) {
 		this.followingLine = line;
 	}
@@ -68,6 +88,14 @@ public class StraightLine {
 		return this.followingLine;
 	}
 	*/
+	/**
+	 * navigation module spots a parking slot during it's scout mode. method evaluates it's parameters, and existence in a current database
+	 * 
+	 * @param ID  id to be given, if the slot is new
+	 * @param frontBoundaryPosition  position of parking slot front boundary, robot passes it second after the back boundary
+	 * @param backBoundaryPosition  position of parking slot back boundary, robot passes it first then the front boundary
+	 * @return  true, if the parking slot added as a new one in the database (and therefore the ID counter has to be incremented)
+	 */
 	public boolean newParkingSlotSpotted(int ID, double frontBoundaryPosition, double backBoundaryPosition) {
 		if ( this.id==5 ) { return false; } // there is no parking slot, robot is turning to a street
 		
@@ -136,27 +164,50 @@ public class StraightLine {
 
 		return slotNotInDatabase;
 	}
-	
+	/**
+	 * further refining of the x, y, coordinates and the robot's angle
+	 * 
+	 * @param pose  current robot pose
+	 * @param perception  module, to use advanced localisation measurements
+	 */
 	public void poseCorrection(Pose pose, IPerception perception) {
 		 
 	}
-	
+	/**
+	 * starting coordinate of this line
+	 * 
+	 * @return  the relevant coordinate one-dimensional. In case of East and West, the x-component, in case of North and South, the y-component
+	 */
 	public double getStartCoordinate() {
 		return this.startCoordinate;
 	}
-	
+	/**
+	 * target coordinate of this line
+	 * 
+	 * @return  the relevant coordinate one-dimensional. In case of East and West, the x-component, in case of North and South, the y-component
+	 */
 	public double getEndCoordinate() {
 		return this.endCoordinate;
 	}
-	
+	/**
+	 * constant component of this line
+	 * 
+	 * @return  the relevant coordinate one-dimensional. In case of North and South, the x-component, in case of East and West, the y-component
+	 */
 	public double getConstCoordinate() {
 		return this.constCoordinate;
 	}
-	
+	/**
+	 * 
+	 * @return  returns this line's direction.
+	 */
 	public Direction getDirection() {
 		return this.direction;
 	}
-	
+	/**
+	 * 
+	 * @return  returns this line's angle relative to the positive x-axis according to its angle
+	 */
 	public double getAngle() {
 		double angle;
 		switch (this.direction) {
@@ -177,7 +228,11 @@ public class StraightLine {
 		}
 		return angle;
 	}	
-	
+	/**
+	 * calculates, whether the robot is switching lines. resets coordinates due to known turning points
+	 * @param pose  current robot pose
+	 * @return  this line or the following line in case the robot is taking a turn
+	 */
 	public StraightLine update( Pose pose ) {
 		double angleDifference = Math.min(
 				Math.abs( this.getAngle() - pose.getHeading() ),
@@ -248,7 +303,10 @@ public class StraightLine {
 			return this;
 		}
 	}
-	
+	/**
+	 * 
+	 * @return  returns a list of parking slot this line is containing
+	 */
 	public List<ParkingSlot> getParkingSlots() {		
 		List<ParkingSlot> parkingSlots = new LinkedList<ParkingSlot>();
 		Point frontBoundaryPosition = null;
